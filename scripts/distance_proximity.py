dc8f0953c78fd2e4c1aa91ed5e68e072fabd5996
Matthew Rush <amata.rush@icloud.com>
2022-09-03 16:03:32 +1000

    school proximity to 1199th row

--- scripts/distance_proximity.py
+++ scripts/distance_proximity.py
@@ -1,10 +1,15 @@
 import openrouteservice
 from geopy import distance
 
+
 def proximity(subject_lat, subject_long, compare_lat, compare_long):
     distances = []
-    for i in range(len(compare_lat)): # B
-        distances.append(distance.distance((subject_lat, subject_long), (compare_lat[i], compare_long[i])).km)
+    for i in range(len(compare_lat)):  
+        distances.append(
+            distance.distance(
+                (subject_lat, subject_long), (compare_lat[i], compare_long[i])
+            ).km
+        )
 
     sorted_distances = sorted(distances, reverse=False)
     min_index_1 = distances.index(sorted_distances[0])
@@ -12,10 +17,13 @@ def proximity(subject_lat, subject_long, compare_lat, compare_long):
 
     return min_index_1, min_index_2
 
+
 def travel_time(subject_lat, subject_long, compare_lat, compare_long, api_key):
 
     client = openrouteservice.Client(key=api_key)  # Specify your personal API key
-    routes = client.directions(((subject_lat, subject_long), (compare_lat, compare_long)))
+    routes = client.directions(
+        ((subject_lat, subject_long), (compare_lat, compare_long))
+    )
 
     distance = routes["routes"][0]["summary"]["distance"]
     duration = routes["routes"][0]["summary"]["duration"]
