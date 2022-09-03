353d06a3b648ea72a243acc584b8f9f1391da63f
Matthew Rush <amata.rush@icloud.com>
2022-09-03 11:46:18 +1000

    school proximity

--- scripts/distance_proximity.py
+++ scripts/distance_proximity.py
@@ -1,10 +1,21 @@
 import openrouteservice
+from geopy import distance
 
+def proximity(subject_lat, subject_long, compare_lat, compare_long):
+    distances = []
+    for i in range(len(compare_lat)): # B
+        distances.append(distance.distance((subject_lat, subject_long), (compare_lat[i], compare_long[i])).km)
 
-def proximity(start, end, api_key):
+    sorted_distances = sorted(distances, reverse=False)
+    min_index_1 = distances.index(sorted_distances[0])
+    min_index_2 = distances.index(sorted_distances[1])
+
+    return min_index_1, min_index_2
+
+def travel_time(subject_lat, subject_long, compare_lat, compare_long, api_key):
 
     client = openrouteservice.Client(key=api_key)  # Specify your personal API key
-    routes = client.directions((start, end))
+    routes = client.directions(((subject_lat, subject_long), (compare_lat, compare_long)))
 
     distance = routes["routes"][0]["summary"]["distance"]
     duration = routes["routes"][0]["summary"]["duration"]
