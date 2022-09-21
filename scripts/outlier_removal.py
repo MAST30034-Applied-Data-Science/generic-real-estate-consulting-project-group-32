3fcfadceeb43f6bd6e6c527bc3471bd8b807f05e
Dylan Beaumont <dbeaumont@student.unimelb.edu.au>
2022-09-21 15:36:58 +1000

    Changed outlier analysis to preserve nans
    Also, changed to impute means

--- scripts/outlier_removal.py
+++ scripts/outlier_removal.py
@@ -3,18 +3,22 @@ from statsmodels.stats.outliers_influence import OLSInfluence
 import statsmodels.regression.linear_model as lm
 
 df = pd.read_csv(f"../data/curated/pre_processed_data.csv")
-# Define the independent variables
-predictors = ["num_beds", "num_baths", "num_parking", "school_distance"]
-# Define the dependent variabe
-target = ["weekly_rent"]
-# Remove NaN values
-df[predictors] = df[predictors].fillna(0)
-df[target] = df[target].fillna(0)
 
 # Remove any value with a weekly rent past 2 standard deviations. (this just removes extreme outliers where data has been input incorrectly)
 mean = df["weekly_rent"].mean()
 std = df["weekly_rent"].std()
 df = df[df["weekly_rent"] <= mean + 2*std]
+
+df2 = df.copy()
+
+# Define the independent variables
+predictors = ["num_beds", "num_baths", "num_parking", "school_distance"]
+# Define the dependent variabe
+target = ["weekly_rent"]
+# Remove NaN values
+df[predictors] = df[predictors].fillna(df[predictors].mean())
+df[target] = df[target].fillna(df[target].mean())
+
 
 # Fit an ordinary linear model
 model = lm.OLS(df[target], df[predictors])
@@ -24,4 +28,4 @@ influence = OLSInfluence(results)
 sres = influence.resid_studentized_internal
 
 # mark points above a certain cooks distance.
-df.loc[influence.cooks_distance[0] < 0.002].to_csv(f"../data/curated/outlier_removed_data.csv")
+df2.loc[influence.cooks_distance[0] < 0.002].to_csv(f"../data/curated/outlier_removed_data.csv")
