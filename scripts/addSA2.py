330a33b15d268628202d0d06ea27b1f409de6e6a
Dylan Beaumont <dbeaumont@student.unimelb.edu.au>
2022-10-16 22:25:27 +1100

    reduce addSA2 column count to 79

--- scripts/addSA2.py
+++ scripts/addSA2.py
@@ -13,23 +13,30 @@ import geoplot.crs as gcrs
 import imageio
 
 def addSA2(df: pd.DataFrame, use_postcode = False):
-    """ Adds SA2 code and geometry of the SA2 value to the dataframe, and converts to GeoDataFrame """
+    """ Adds SA2 code and geometry of the SA2 value to the dataframe, and 
+    converts to GeoDataFrame. Use postcode specifies whether to use the 
+    postcode or latitude and longitude.
+    """
     
     # If missing longitude and latitude
     if use_postcode:
         postcodes = pd.read_csv("../data/raw/postcode.csv")
-        postcodes = postcodes.rename(columns={"lat":"latitude", "long":"longitude"})
+        postcodes = postcodes.rename(columns={"lat":"latitude",  \
+                                              "long":"longitude"})
         postcodes = postcodes[["postcode", "latitude", "longitude"]]
         postcodes = postcodes.drop_duplicates()
-        df = df.join(postcodes.set_index("postcode"), on = "postcode", lsuffix = "_l", rsuffix = "")
+        df = df.join(postcodes.set_index("postcode"), on = "postcode",  \
+                     lsuffix = "_l", rsuffix = "")
 
     shape = gpd.read_file('../data/raw/ShapeFile/SA2_2021_AUST_GDA2020.shp')
     shape = shape.loc[shape.STE_NAME21 == "Victoria"]
     shape = shape.loc[shape.geometry != None]
     shape = shape[["SA2_CODE21", "geometry", "AREASQKM21"]]
     shape = shape.astype({"SA2_CODE21": float})
-    df["SA2"] = df.apply(lambda x: find_zone(x["longitude"],x["latitude"],shape), axis=1)
-    geo_df = gpd.GeoDataFrame(df.join(shape.set_index("SA2_CODE21")["geometry"], on = "SA2"))
+    df["SA2"] = df.apply(lambda x: find_zone(x["longitude"],  \
+                                             x["latitude"],shape), axis=1)
+    geo_df = gpd.GeoDataFrame(df.join(shape.set_index("SA2_CODE21")  \
+                                      ["geometry"], on = "SA2"))
     
     return geo_df
 
@@ -38,7 +45,8 @@ def find_zone(long, lat, shape_df):
     """ Finds the SA2 value based on the coordinates"""
     if np.isnan(lat) or np.isnan(long):
         return 0
-    sa2 = shape_df.loc[shape_df["geometry"].contains(Point(long,lat)), "SA2_CODE21"]
+    sa2 = shape_df.loc[shape_df["geometry"].contains(Point(long,lat)),  \
+                       "SA2_CODE21"]
     if len(sa2) != 1:
         return 0
     else: 
